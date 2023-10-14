be40dfff652020683b31507ce5cf796c9aa02d87
Hamza El mesaouab <77578145+Hamza-MS@users.noreply.github.com>
2023-10-14 15:31:36 +0100

    cleaning budget service and contorler for a complete rework

--- src/main/java/ma/ac/uir/projets8/service/BudgetService.java
+++ src/main/java/ma/ac/uir/projets8/service/BudgetService.java
@@ -23,42 +23,5 @@ public class BudgetService {
 
     private final BudgetRepository budgetRepository;
 
-    //TODO: khassou rework kamel
 
-    public Budget createBudget(Budget budget) {
-        return budgetRepository.save(budget);
-    }
-
-    public Optional<Budget> getBudgetById(Long id) {
-        return budgetRepository.findById(id);
-    }
-
-    public List<Budget> getAllBudgets() {
-        return budgetRepository.findAll();
-    }
-
-    public Budget updateBudget(Long id, Budget budget) {
-        return budgetRepository.findById(id)
-                .map(updatedBudget -> {
-                    updatedBudget.setBudget_initial(budget.getBudget_initial());
-                    updatedBudget.setBudget_restant(budget.getBudget_restant());
-                    return budgetRepository.save(budget);
-                })
-                .orElseThrow(() -> new BudgetNotFoundException(id));
-    }
-
-    public void deleteBudget(Long id) {
-        if (!budgetRepository.existsById(id)) {
-            throw new BudgetNotFoundException(id);
-        }
-        budgetRepository.deleteById(id);
-    }
-
-    public ResponseEntity<List<Transaction>> getTransactionsByBudget(Long id){
-        try {
-            return ResponseEntity.ok(new ArrayList<>(budgetRepository.findById(id).orElseThrow(() -> new BudgetNotFoundException(id)).getTransactions()));
-        }catch (MeetingNotFoundException e){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
-        }
-    }
 }
