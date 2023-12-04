1d3cc43822499d8e60340b50cf86a25aaf5abdad
yassine <yassinemouddene@gmail.com>
2023-12-04 14:20:17 +0100

    fix : seperated file generation logic to a seperate class

--- src/main/java/ma/ac/uir/projets8/service/ClubService.java
+++ src/main/java/ma/ac/uir/projets8/service/ClubService.java
@@ -17,9 +17,6 @@ import ma.ac.uir.projets8.repository.EventRepository;
 import ma.ac.uir.projets8.repository.PersonnelRepository;
 import ma.ac.uir.projets8.repository.StudentRepository;
 import org.apache.commons.lang3.ObjectUtils;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.dhatim.fastexcel.Workbook;
-import org.dhatim.fastexcel.Worksheet;
 import org.dhatim.fastexcel.reader.ReadableWorkbook;
 import org.dhatim.fastexcel.reader.Row;
 import org.dhatim.fastexcel.reader.Sheet;
@@ -42,6 +39,8 @@ import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.*;
 import java.util.stream.Stream;
+
+import static ma.ac.uir.projets8.util.FileGenerator.generateStudentFile;
 
 @RequiredArgsConstructor
 @Service
@@ -203,52 +202,8 @@ public class ClubService {
 
         try {
             List<Student> members = new ArrayList<>(clubRepository.findById(id).orElseThrow(() -> new ClubNotFoundException(id)).getMembers());
-            String path = "C:/Users/yassi/Bureau/projet-s8-file-transfer/temp/rapport-"
-                    + new SimpleDateFormat("dd-MM-yyyy").format(new Date()) + ".xlsx";
-            try (OutputStream os = new FileOutputStream(path)) {
-
-                Workbook wb = new Workbook(os, "MyApplication", "1.0");
-                Worksheet ws = wb.newWorksheet("sheet 1");
-                ws.range(0, 0, members.size(), 3).createTable()
-                        .setName("test")
-                        .setDisplayName("display name")
-                        .styleInfo();
-                ws.value(0, 0, "Student id");
-                ws.value(0, 1, "first name");
-                ws.value(0, 2, "last name");
-                ws.value(0, 3, "email");
-                ws.value(0, 4, "major");
-                ws.value(0, 5, "level");
-                for (int i = 0; i < members.size(); i++) {
-                    ws.value(i + 1, 0, members.get(i).getFirstName());
-                    ws.value(i + 1, 1, members.get(i).getLastName());
-                    ws.value(i + 1, 2, members.get(i).getEmail());
-                    ws.value(i + 1, 3, members.get(i).getStudentId());
-                    ws.value(i + 1, 4, members.get(i).getMajor());
-                    ws.value(i + 1, 5, members.get(i).getLevel());
-                }
-                ws.finish();
-                wb.finish();
-
-
-                File file = new File(path);
-                response.setContentType("application/vnd.ms-excel");
-                response.setHeader("Content-disposition", "attachment; filename=" + file.getName());
-
-                OutputStream out = response.getOutputStream();
-                FileInputStream in = new FileInputStream(file);
-
-                // copy from in to out
-                IOUtils.copy(in, out);
-
-                out.close();
-                in.close();
-                os.close();
-                file.delete();
-
-            } catch (Exception e) {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
-            }
+            String fileName = "rapport-" + new SimpleDateFormat("dd-MM-yyyy").format(new Date()) + ".xlsx";
+            generateStudentFile(response, members, fileName);
 
         } catch (ClubNotFoundException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
